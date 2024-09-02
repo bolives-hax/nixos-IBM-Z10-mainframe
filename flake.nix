93051a062ac98226508f2df789c5bf96620de174
bl0v3 <dunno@uh.tahsthsat>
2024-09-02 14:45:13 +0000

    crude-b4-cleanup

--- flake.nix
+++ flake.nix
@@ -1,11 +1,8 @@
 {
   inputs = {
     nixpkgs = {
-    #  #url = "github:bolives-hax/nixpkgs/s390-tools-testing-staging-cython-fix";
-       #url = "path:/home/flandre/nixos-IBM-Z10-mainframe/nixpkgs";
-      url = "path:/tmp//stdenv-nixpkgs-commit";
-      #url = "github:bolives-hax/nixpkgs/add-s390x-installer-cdrom-test";
-    #  
+       #url = "path:/home/flandre/nixpkgs-p";
+       url = "/tmp/nixpkgs-p"; 
     };
     libfuse = {
       url = "github:bolives-hax/libfuse/s390x-fix";
@@ -15,40 +12,53 @@
   outputs = { self, nixpkgs, libfuse}: {
     nixosConfigurations = {
       z10 = nixpkgs.lib.nixosSystem {
+	system = "s390x-linux";
         modules = [
           # make a tarball
           ({pkgs,config,modulesPath,lib,...}: {
-		nixpkgs.hostPlatform = ({ system = "s390x-linux"; } // lib.systems.platforms.z10);
-            #system.build.tarball = pkgs.callPackage "${modulesPath}/../lib/make-system-tarball.nix" {
-            #  extraArgs = "--owner=0";
+		nixpkgs.hostPlatform = ({
+			system = "s390x-linux"; 
+			#rustcTarget = "
+			linux-kernel = {
+			      	name = "s390x";
+      				baseConfig = "defconfig";
+      				target = "bzImage";
+      				autoModules = true;
+			};
+			gcc.arch = "z10";
+
+		} ); # // lib.systems.platforms.z10);
+		
+            system.build.tarball = pkgs.callPackage "${modulesPath}/../lib/make-system-tarball.nix" {
+              extraArgs = "--owner=0";
 
 
-            #  storeContents = [
-            #    {
-            #      object = config.system.build.toplevel;
-            #      symlink = "none";
-            #    }
-            #  ];
+              storeContents = [
+                {
+                  object = config.system.build.toplevel;
+                  symlink = "none";
+                }
+              ];
 
 
 
-            #  contents = [
-            #    {
-            #      source = config.system.build.toplevel + "/init";
-            #      target = "/sbin/init";
-            #    }
-            #    # Technically this is not required for lxc, but having also make this configuration work with systemd-nspawn.
-            #    # Nixos will setup the same symlink after start.
-            #    {
-            #      source = config.system.build.toplevel + "/etc/os-release";
-            #      target = "/etc/os-release";
-            #    }
-            #  ];
-            #  # TB
+              contents = [
+                {
+                  source = config.system.build.toplevel + "/init";
+                  target = "/sbin/init";
+                }
+                # Technically this is not required for lxc, but having also make this configuration work with systemd-nspawn.
+                # Nixos will setup the same symlink after start.
+                {
+                  source = config.system.build.toplevel + "/etc/os-release";
+                  target = "/etc/os-release";
+                }
+              ];
+              # TB
 
 
-            #  extraCommands = "mkdir -p proc sys dev";
-            #};
+              extraCommands = "mkdir -p proc sys dev";
+            };
           })
           ({ config, pkgs, lib, ... }: {
             boot = {
@@ -101,14 +111,37 @@
 	   */
             # ... extra configs as above
             #services.xserver.enable = true;
+	/*
             nixpkgs.buildPlatform.system =
-              "x86_64-linux"; # If you build on x86 other wise changes this.
+              #"x86_64-linux"; # If you build on x86 other wise changes this.
+		"s390x-linux";
+	*/
 
             imports = [
               #"${nixpkgs}/nixos/modules/profiles/headless.nix"
               "${nixpkgs}/nixos/modules/profiles/minimal.nix"
               "${nixpkgs}/nixos/modules/installer/cd-dvd/iso-image-s390x.nix"
             ];
+	
+	    users.users.flandre = {
+	        group = "flandre";
+		extraGroups = [ "wheel" ];
+		isNormalUser = true;
+	    };
+
+	   /*security.sudo.extraRules = [
+ 		 {
+		   users = [ "flandre" ];
+ 		   commands = [
+			 "ALL" "NOPASSWD" 
+		   ];
+ 		 }
+	   ];*/
+	    security.sudo.wheelNeedsPassword = false;
+
+   	    users.groups.flandre = {};
+
+ 	    services.getty.autologinUser = "flandre";
 
             #services.nginx = {
             #	enable = true;
@@ -138,15 +171,16 @@
             # only add strictly necessary modules
             #boot.initrd.includeDefaultModules = false;
             #boot.initrd.kernelModules = [ "ext4" ];
+            boot.initrd.compressor = "cat"; 
             disabledModules = [
-              #"${nixpkgs}/nixos/modules/profiles/all-hardware.nix"
-              #"${nixpkgs}/nixos/modules/profiles/base.nix"
+              "${nixpkgs}/nixos/modules/profiles/all-hardware.nix"
+              "${nixpkgs}/nixos/modules/profiles/base.nix"
             ];
 
             # disable useless software
-            #xdg.icons.enable  = false;
-            #xdg.mime.enable   = false;
-            #xdg.sounds.enable = false;
+            xdg.icons.enable  = false;
+            xdg.mime.enable   = false;
+            xdg.sounds.enable = false;
           })
         ];
       };
